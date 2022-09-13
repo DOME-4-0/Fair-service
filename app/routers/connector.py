2df778ddeec498e97e028b77af48f47d03d25c2c
Mike <mike@Gerdesktop.localdomain>
2022-09-13 15:42:10 +0100

    changes to the connector file

--- app/routers/connector.py
+++ app/routers/connector.py
@@ -1,11 +1,15 @@
+"""Connector service"""
 import json
 import re
-from typing import List, Optional
-import periodictable
-import requests
-from fastapi import APIRouter, Query
+from typing import List
+import periodictable  # type: ignore
+import requests  # type: ignore
+from fastapi import APIRouter
+from operator import itemgetter
 
 router = APIRouter()
+
+# pylint:disable=line-too-long
 
 
 @router.get("/test")
@@ -13,139 +17,158 @@ async def test():
     """
     test endpoint
     """
-    return {"msg": "Hello from the connector service!"}
-
-
-
-#@router.get("/platforms")
-#async def platform_name(platform_string:str):
-#    Available_platform = ['Crystallography','Novel Materials Discovery','AFLOW',
-#    'Theoretical Crystallography Open Database',
-#    'The Open Quantum Materials', 'Materials Project','2DMatpedia','Open Database Xtals',"Pubchem"]
-#
-#    if platform_string == "":
-#        return {"msg":"No platform has been entered! Please enter a valid platform"}
-#
-#    elif platform_string not in Available_platform:
-#        return {"msg":"The entered platform is either not valid or not available for search on DOME yet"}
-#
-#    elif platform_string in Available_platform:
-#        return platform_string.lower()
-#    #You should create a script to check the version based URLs, this helps the later querying.
-#    #The version endpoint in Optimade could help with that.
+    return {"msg": "Hello World from connector service"}
 
 
 @router.get("/results")
 async def search_results(
-    search_string: Optional[str] = Query(None),
-    platform_name: Optional[str] = Query(None),
-    results_type: Optional[str] = Query(None)):
-
-    search_url_base = Search_API(platform_name, results_type)
+        search_string: str,
+        platform_name: str):
+    """helps to connect to data platforms"""
+    search_url_base = search_api(platform_name)
     print(search_url_base)
-    searched_results=[]
+    searched_results = []
 
-    if platform_name =="Pubchem":
-        res = requests.get(f"{search_url_base}{search_string}/record/JSON?callback=pubchem_callback",
-        params=dict(page_limit=10)).json()
-
+    if platform_name == "Pubchem":
+        base_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{search_string}/record/JSON?callback=pubchem_callback"
+        searched_results = []
+        res_base = requests.get(base_url, params=dict(page_limit=10)).json()
+        # print(res)
+        chemical_pubchem_results = res_base["PC_Compounds"][0]
+        cid_id = chemical_pubchem_results.get(
+            'id', {}).get('id', {}).get('cid')
+        pubchem_chem_hazard_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{cid_id}/JSON/?response_type=display&heading=GHS%20Classification"
+        res_chemical_hazard = requests.get(
+            pubchem_chem_hazard_url, params=dict(page_limit=10)).json()
+        GHZ_hazard_statements = res_chemical_hazard['Record']['Section'][0][
+            'Section'][0]['Section'][0]['Information'][2]['Value']['StringWithMarkup']
+        # print(GHZ_hazard_statements)
+        property_pubchem = chemical_pubchem_results.get('props')
+        name = property_pubchem[11]["value"]["sval"]
+        name = ' '.join(w for w in re.split(r"\W", name)
+                        if w)  # pylint: disable=W1401
+        item = {
+            "keyword": name,
+            "dataCreator": "PubChem",
+            "URL": base_url,
+            "Hazard_information": json.dumps(GHZ_hazard_statements, sort_keys=True, indent=4),
+            "data": json.dumps(itemgetter('atoms', 'coords', 'props')(chemical_pubchem_results), sort_keys=True, indent=4)
+        }
+        searched_results.append(item)
     elif platform_name == "Chemeo":
-        res = requests.get(f"{search_url_base}?q={search_string}").json()
+        try:
+            search_url_chemeo = f"{search_url_base}?q={search_string}"
+            res = requests.get(search_url_chemeo).json()
+            if res["compounds"]:
+                for compound in res["compounds"]:
+                    # print(compound["other_names"][0])
+                    name = compound["other_names"][0]
+                    print(name)
+                    item = {
+                        "keyword": name,
+                        "dataCreator": "Chemeo",
+                        "URL": f"https://www.chemeo.com/api/v1/search?q={name}",
+                        "data": json.dumps(compound, sort_keys=True, indent=4),
+                    }
+                    searched_results.append(item)
+        except KeyError:
+            pass
     else:
-        elements =search_string_split(search_string)
+        elements = search_string_split(search_string)
         elements_str = ", ".join([f'"{el}"' for el in elements])
         filters = f"(elements HAS ALL {elements_str})"
-        res = requests.get(search_url_base, params=dict(filter=filters, page_limit=10)).json()
-    try:
-        for entry in res["data"]:
-            attrs = entry["attributes"]
-            item = {
-                "keyword": attrs["chemical_formula_descriptive"],
-                "dataCreator": platform_name,
-                "URL": search_url_base+entry["id"],
-                "data": json.dumps(entry, sort_keys=True, indent=4),
-            }
-            searched_results.append(item)
-    except:
-        return []
+        res = requests.get(search_url_base, params=dict(
+            filter=filters, page_limit=10)).json()
+        try:
+            for entry in res["data"]:
+                attrs = entry["attributes"]
+                item = {
+                    "keyword": attrs["chemical_formula_descriptive"],
+                    "dataCreator": platform_name,
+                    "URL": search_url_base+str(entry["id"]),
+                    "data": json.dumps(entry, sort_keys=True, indent=4),
+                }
+                searched_results.append(item)
+        except ValueError:
+            return []
     return searched_results
 
-def searched_platform(platform):
-    if platform == "":
-        return print("No platform has been entered! Please enter a valid platform")
 
-    if platform != "":
-        return platform.lower()
-
-def search_string_split(search_string:str) -> List:
+def search_string_split(search_string: str) -> List:
+    """Split the search string"""
     elements = []
-    for term in [a for a in re.split(",|\s+", search_string.strip()) if a]:
+    for term in [a for a in re.split(",|\s+", search_string.strip()) if a]:  # pylint: disable=W1401
         try:
             elements.append(periodictable.elements.name(term.lower()))
         except ValueError:
-            pass
+            print(" ")
 
-        pass
+        print(" ")
     return elements
 
 
-def Search_API (platform_name, results_type):
-    #Optimade query URLs:
-    #(Not Worked) Materials Platform for Data Science: https://api.mpds.io
-    #(Not worked) Open Materials Database: http://optimade.openmaterialsdb.se
+def search_api(platform_name):
+    """Generate the API automatically"""
+    # Optimade query URLs:
+    # (Not Worked) Materials Platform for Data Science: https://api.mpds.io
+    # (Not worked) Open Materials Database: http://optimade.openmaterialsdb.se
 
-    #Common Endpoints : Links, references, structures, info
+    # Common Endpoints : Links, references, structures, info
 
-    #(Worked) MaterialsProject : https://optimade.materialsproject.org
-    #(Worked) 2DMatpedia: http://optimade.2dmatpedia.org
-    #(Worked) Open Database Xtals: http://optimade.openmaterialsdb.se
-    #(Worked) Crystallography: http://crystallography.net/cod/optimade
-    #(Worked) Novel Materials Discovery (NOWAD): https://nomad-lab.eu/prod/rae/optimade
-    #(Worked) AFLOW: http://aflow.org/API/optimade
-    #(Worked) Theoretical Crystallography Open Database: https://www.crystallography.net/tcod/optimade
-    #(Worked) The Open Quantum Materials: http://oqmd.org/optimade
-    #(Worked) JARVIS-DFT: https://jarvis.nist.gov/optimade/jarvisdft
+    # (Worked) MaterialsProject : https://optimade.materialsproject.org
+    # (Worked) 2DMatpedia: http://optimade.2dmatpedia.org
+    # (Worked) Open Database Xtals: https://optimade.odbx.science
+    # (Worked) Crystallography: http://crystallography.net/cod/optimade
+    # (Worked) Novel Materials Discovery (NOWAD): https://nomad-lab.eu/prod/rae/optimade
+    # (Worked) AFLOW: http://aflow.org/API/optimade
+    # (Worked) Theoretical Crystallography Open Database: https://www.crystallography.net/tcod/optimade
+    # (Worked) The Open Quantum Materials: http://oqmd.org/optimade
+    # (Worked) JARVIS-DFT: https://jarvis.nist.gov/optimade/jarvisdft
     # In total, four types of results can be returned from the queries, links, references, structures, info.
-    optimade_platform_front ={'Materials Project':'materialsproject.org', '2DMatpedia':'2dmatpedia.org','Open Database Xtals':'openmaterialsdb.se'}
-    optimade_platform_end={'Crystallography':'crystallography.net/cod','Novel Materials Discovery':'nomad-lab.eu/prod/rae','AFLOW':'aflow.org/API',
-    'Theoretical Crystallography Open Database':'crystallography.net/tcod',
-    'The Open Quantum Materials':'oqmd.org'}
-
-    optimade_results_types = ['structures','references','info','Links']
+    optimade_platform_front = {'Materials Project': 'materialsproject.org',
+                               'Open Database Xtals': 'odbx.science'}
+    optimade_platform_end = {'Novel Materials Discovery': 'nomad-lab.eu/prod/rae', 'AFLOW': 'aflow.org/API',
+                             'The Open Quantum Materials': 'oqmd.org'}
+    new_optimade_cod_api = {'Theoretical Crystallography Open Database': 'crystallography.net/tcod',
+                            'Crystallography': 'crystallography.net/cod'}
+    #optimade_results_types = ['structures', 'references', 'info', 'Links']
     if platform_name in optimade_platform_front:
         platform_string = optimade_platform_front.get(platform_name)
-        query_base_url = f"https://optimade.{platform_string}/v1/"
+        query_base_url = f"https://optimade.{platform_string}/v1/structures"
         print(query_base_url)
 
     elif platform_name in optimade_platform_end:
         platform_string = optimade_platform_end.get(platform_name)
-        query_base_url = f"https://{platform_string}/optimade/v1/"
+        query_base_url = f"https://{platform_string}/optimade/v1/structures"
 
-    if results_type == "structures":
-        query_base_url= query_base_url+"structures"
-
-    if results_type == "references":
-        query_base_url_references= query_base_url+"references"
-
-    if results_type == "info":
-        query_base_url_info= query_base_url+"info"
-
-    if results_type == "Links":
-        query_base_url_links= query_base_url+"links"
+    elif platform_name in new_optimade_cod_api:
+        platform_string = new_optimade_cod_api.get(platform_name)
+        query_base_url = f"https://www.{platform_string}/optimade/v1.1.0/structures"
+    # if results_type == "references":
+    #    query_base_url_references = query_base_url+"references"
+#
+    # if results_type == "info":
+    #    query_base_url_info = query_base_url+"info"
+#
+    # if results_type == "Links":
+    #    query_base_url_links = query_base_url+"links"
 
     elif platform_name == 'JARVIS-DFT':
-        query_base_url = "https://jarvis.nist.gov/optimade/jarvisdft/v1/"
+        query_base_url = "https://jarvis.nist.gov/optimade/jarvisdft/v1/structures"
 
-
+    # elif platform_name == '2DMatpedia':
+    #   query_base_url = "https://www.optimade.2dmatpedia.org/v1/structures"
     elif platform_name == "Pubchem":
         query_base_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
 
     elif platform_name == "Chemeo":
-        query_base_url =  "https://www.chemeo.com/api/v1/search"
+        query_base_url = "https://www.chemeo.com/api/v1/search"
+
+    else:
+        query_base_url = " "
 
     return query_base_url
 
 
-
-#print(Search_API(platform_name='Materials Project',results_type='structures'))
+#print(search_api(platform_name='Materials Project',results_type='structures'))
 #print(searching(search_string ='carbon',platform_name='JARVIS-DFT',results_type='structures'))
--- tests/test_connector.py
+++ tests/test_connector.py
@@ -5,6 +5,6 @@ app = create_app()
 client = TestClient(app)
 
 def test_read_home():
-    response = client.get("/")
+    response = client.get("/test")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World from connector service"}
